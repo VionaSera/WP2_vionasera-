7d93a1720661e02428a7c5b16d2799a6c7aee068
VionaSera <vionasera7@gmail.com>
2022-03-21 01:04:52 +0700

    isian tugas

---
+++ pustaka-booking/application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php 
+class Contoh1 extends CI_controller
+{
+    public function index()
+{
+  echo "<h1>perkenalan</h1>";
+  echo"Nama saya Viona Sera 
+       Saya tinggal di bogor
+       olahraga yang saya sukai adalah 
+       bola volly";
+ }
+}
